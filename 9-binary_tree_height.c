0b595104716499c84126bec69d0d1585b97debec
Le-Meur-Mai <mai.lm@outlook.fr>
2025-08-05 10:42:08 +0200

    Correction of a wrong output when the binary tree was NULL now return 0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,14 +11,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_r = 0;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		if (tree->left != NULL)
+			height_l = binary_tree_height(tree->left);
+		else
+			height_l = 0;
+		if (tree->right != NULL)
+			height_r = binary_tree_height(tree->right);
+		else
+			height_r = 0;
 
-	height_l = binary_tree_height(tree->left) + 1;
-	height_r = binary_tree_height(tree->right) + 1;
-
-	if (height_l > height_r)
-		return (height_l);
+		if (height_l > height_r)
+			return (height_l + 1);
+		else
+			return (height_r + 1);
+	}
 	else
-		return (height_r);
+		return (0);
 }
